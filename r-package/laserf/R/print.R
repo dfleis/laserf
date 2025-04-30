203ae481ea1a5afc9e7364fa5535dbdea19c0045
dfleis <david.p.fleischer@gmail.com>
2025-04-30 08:02:35 -0400

    restoring tree printing function

--- r-package/laserf/R/print.R
+++ r-package/laserf/R/print.R
@@ -21,3 +21,57 @@ print.laserf <- function(x, decay.exponent = 2, max.depth = 4, ...) {
   cat("Variable importance:", "\n")
   print(var.importance)
 }
+
+
+#' Print a LASERF tree object.
+#' @param x The tree to print.
+#' @param ... Additional arguments (currently ignored).
+#'
+#' @method print laserf_tree
+#' @export
+print.laserf_tree <- function(x, ...) {
+  cat("GRF tree object", "\n")
+  cat("Number of training samples:", x$num_samples, "\n")
+  cat("Variable splits:", "\n")
+
+  # Add the index of each node as an attribute for easy access.
+  nodes <- lapply(1:length(x$nodes), function(i) {
+    node <- x$nodes[[i]]
+    node$index <- i
+    return(node)
+  })
+
+  # Perform DFS to print the nodes (mimicking a stack with a list).
+  frontier <- nodes[1]
+  frontier[[1]]$depth <- 0
+  while (length(frontier) > 0) {
+    # Pop the first node off the stack.
+    node <- frontier[[1]]
+    frontier <- frontier[-1]
+
+    output <- paste(rep("  ", node$depth), collapse = "")
+    output <- paste(output, "(", node$index, ")", sep = "")
+
+    if (node$is_leaf) {
+      leaf_stats_text <- ""
+      if (!is.null(node$leaf_stats)) {
+        leaf_stats_text <- paste(paste(names(node$leaf_stats), unname(node$leaf_stats), sep = ": ", collapse = " "))
+      }
+      output <- paste(output, "* num_samples:", length(node$samples), "", leaf_stats_text)
+    } else {
+      split.var <- node$split_variable
+      split.var.name <- x$columns[split.var]
+      output <- paste(output, "split_variable:", split.var.name, " split_value:", signif(node$split_value))
+
+      left_child <- nodes[node$left_child]
+      left_child[[1]]$depth <- node$depth + 1
+
+      right_child <- nodes[node$right_child]
+      right_child[[1]]$depth <- node$depth + 1
+
+      frontier <- c(left_child, right_child, frontier)
+    }
+    cat(output, "\n")
+  }
+}
+
